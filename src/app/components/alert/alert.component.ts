4970c25874dd1ec091496117a3db010dcad70e5e
cristian.sterie <cristian.sterie@technohub.ro>
2026-02-23 18:02:47 +0200

    EE9: fixed;

--- src/app/components/alert/alert.component.ts
+++ src/app/components/alert/alert.component.ts
@@ -1,11 +1,11 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-alert',
   templateUrl: './alert.component.html',
   styleUrls: ['./alert.component.scss'],
 })
-export class AlertComponent {
+export class AlertComponent implements OnChanges {
   @Input() message = '';
   @Input() type: 'success' | 'danger' | 'warning' = 'warning';
 
@@ -13,7 +13,11 @@ export class AlertComponent {
     // empty constructor
   }
 
-  clearMessage() {
-    this.message = '';
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['message'] && this.message) {
+      setTimeout(() => {
+        this.message = '';
+      }, 3000); // auto-hide after 3 seconds
+    }
   }
 }
